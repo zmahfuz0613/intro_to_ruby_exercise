dc95b2d3196205a26a32d1711056cb1b6ae655ea
Svetla Syrimis <svetla.syrimis@gmail.com>
2019-11-11 15:07:11 -0500

    Update student_solution.rb

---
+++ student_solution.rb
@@ -0,0 +1,121 @@
+# Hello World
+
+#1
+
+
+#2
+
+
+#3
+
+
+
+# Nums Array and Enumerables
+
+#1
+
+
+#2
+
+
+#3
+
+
+#4
+
+
+#5
+
+
+#6
+
+
+#7
+
+
+#8
+
+
+#9
+
+
+#10
+
+
+#11
+
+
+#12
+
+
+#13
+
+
+#14
+
+
+#15
+
+
+#16
+
+
+
+
+# Color Array
+
+#1
+
+
+#2
+
+
+#3
+
+
+# Methods
+
+#1 
+
+def find_area height, width
+
+end
+
+#2
+nums = [5, 5, 6, 7, 2, 4, 3, 2, 1, 9, 7, 6, 0, 0]
+
+def multiply_each_by_five arr
+
+end
+
+
+# Methods With a Hash
+
+book = {
+  title: 'The Great Gatsby',
+  author: 'F Scott Fitzgerald',
+  year: 1925,
+  price: 10
+}
+
+lamp = {
+  type: 'reading',
+  brand: 'Ikea',
+  price: 25
+}
+
+table = {
+  type: 'bed side',
+  brand: 'Crate & Barrel',
+  color: 'birch',
+  price: 50
+}
+
+def print_price hash
+  
+end
+
+
+def print_item_sums(hash1, hash2)
+  
+end
